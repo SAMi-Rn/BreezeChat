069793ccf451ae5df7f8630ee328a9b9f6c603a4
SAMi-Rn <sami_roudgarian@yahoo.com>
2024-03-17 00:45:57 -0700

    auth.controller.js setup

--- backend/routes/auth.routes.js
+++ backend/routes/auth.routes.js
@@ -1,17 +1,12 @@
 import express from 'express'
+import { signup, login, logout } from '../controllers/auth.controller.js';
 
-const router = express.Router()
+const router = express.Router();
 
-router.get('/signup', (req, res) => {     
-    res.send('Signup')
-})  
+router.get('/signup');
 
-router.get('/login', (req, res) => {
-    res.send('Login')
-})
+router.get('/login');
 
-router.get('/logout', (req, res) => {
-    res.send('Logout')
-})
+router.get('/logout');
 
 export default router;
