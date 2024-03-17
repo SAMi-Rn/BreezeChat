59615db51522b3d0c016df93f6da2ece4c11d8fd
SAMi-Rn <sami_roudgarian@yahoo.com>
2024-03-17 00:41:58 -0700

    auth.routes.js setup

--- backend/server.js
+++ backend/server.js
@@ -1,6 +1,7 @@
 import express from "express";
 import dotenv from "dotenv";
 
+import authRoutes from './routes/auth.routes.js';
 const app = express();
 dotenv.config();
 const PORT = process.env.PORT || 5000
@@ -8,6 +9,9 @@ const PORT = process.env.PORT || 5000
 app.get("/", (req, res) => {
     res.send("Server is ready");
 });
+
+app.use("/api/auth",authRoutes);
+
 app.listen(PORT, () => {
 	console.log(`Server Running on port ${PORT}`);
 });
