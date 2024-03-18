fd8b0c34b902ea59404100a666b25416e4e15f7c
SAMi-Rn <sami_roudgarian@yahoo.com>
2024-03-17 23:33:56 -0700

    send Message controller without socket IO

--- backend/server.js
+++ backend/server.js
@@ -1,7 +1,9 @@
 import express from "express";
 import dotenv from "dotenv";
+import cookieParser from "cookie-parser";
 
 import authRoutes from './routes/auth.routes.js';
+import msgRoutes from './routes/message.routes.js';
 import connect from "./db/connect.js";
 
 const app = express();
@@ -10,7 +12,10 @@ const PORT = process.env.PORT || 5100
 dotenv.config();
 
 app.use(express.json());
+app.use(cookieParser());
+
 app.use("/api/auth", authRoutes);
+app.use("/api/messages", msgRoutes);
 
 app.get("/", (req, res) => {
     res.send("Server is ready");
