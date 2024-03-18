a3dca0c3955255d713a2237d85a966b7d228ff1b
SAMi-Rn <sami_roudgarian@yahoo.com>
2024-03-17 22:50:25 -0700

    loginin and logout setup

--- backend/controllers/auth.controller.js
+++ backend/controllers/auth.controller.js
@@ -40,17 +40,45 @@ export const signup = async (req, res) => {
             return res.status(500).send("Error creating user")    
        }
    } catch (error) {
-       console.log(error.message);
+       console.log('error in signup controller');
        res.status(500).json({ message: error.message });
    }
 };
 
-export const login = (req, res) => {
-    res.send("Login route")
+export const login = async(req, res) => {
+    try {
+        const { userName, password } = req.body;
+        const user = await User.findOne({ userName });
+        // need || for becrypt to work properly for non existing user
+        const validPassword = await bcrypt.compare(password, user?.password || "");
+        if (!user) {
+            return res.status(400).send("Invalid username");
+        }
+        if (!validPassword) {
+            return res.status(400).send("Invalid password");
+        }
+        generateTokenAndSetCookie(res, user._id);
+        res.status(200).json({
+            _id: user._id,
+            firstName: user.firstName,
+            lastName: user.lastName,
+            userName: user.userName,
+            profilePicture: user.profilePicture,
+        });
+    } catch (error) {
+        console.log('error in signup controller');
+        res.status(500).json({ message: error.message });
+    }
 }
 
 export const logout = (req, res) => {
-    res.send("Logout route")
+    try {
+        res.cookie("token", "", {maxAge: 0});
+        res.status(200).json({ message: "Logged out successfully" });
+    } catch (error) {
+        console.log('error in logout controller');
+        res.status(500).json({ message: error.message });
+    }
 };
 
 
